dddffe1fbbcf3dbf4024e2d7c93962582bd97d5a
karu <nimetu@gmail.com>
2018-01-05 14:23:58 +0200

    Changed: refactor memory usage test

--- tests/memory_usage.php
+++ tests/memory_usage.php
@@ -1,5 +1,7 @@
 <?php
 
+ini_set('memory_limit', '-1');
+
 require_once __DIR__.'/../ryzom_extra.php';
 
 function load_datasets(array $array) {
@@ -8,47 +10,43 @@ function load_datasets(array $array) {
 	$_mem = memory_get_peak_usage();
 	$_time = microtime(true);
 	foreach ($array as $ds) {
-		echo "{$ds} ";
-		$cache[$ds] = ryzom_extra_load_dataset(RYZOM_EXTRA_SHEETS_CACHE.'/'.$ds.'.serial');
+		$fname = RYZOM_EXTRA_SHEETS_CACHE.'/'.$ds.'.serial';
+		if (file_exists($fname)) {
+			echo "{$ds} ";
+			$cache[$ds] = ryzom_extra_load_dataset($fname);
+		} else {
+			echo "!{$dst}";
+		}
 	}
 	echo "\n";
 	printf("% 15s bytes\n", number_format(memory_get_peak_usage() - $_mem, 2, '.', ','));
 }
 
+function group_datasets($dir) {
+	$files = glob($dir);
+
+	$groups = array();
+	foreach($files as $file) {
+		$fname = basename($file);
+		$fname = substr(basename($file), 0, strrpos($fname, '.'));
+		if (strstr($fname, 'words_')) {
+			$group = substr($fname, 0, strlen('words_??'));
+		} elseif (strstr($fname, '-') !== false) {
+			$group = substr($fname, 0, strpos($fname, '-'));
+		} else {
+			$group = $fname;
+		}
+		$groups[$group][] = $fname;
+	}
+	return $groups;
+}
 
 echo "//\n// dataset memory usage\n//\n";
-load_datasets([
-	'words_en_creature',
-	'words_en_faction',
-	'words_en_item',
-	'words_en_outpost',
-	'words_en_outpost_building',
-	'words_en_outpost_squad',
-	'words_en_place',
-	'words_en_sbrick',
-	'words_en_skill',
-	'words_en_sphrase',
-	'words_en_title',
-	'words_en_uxt',
-]);
-load_datasets(['craftplan']);
-load_datasets(['items']);
-load_datasets(['resource_stats']);
-load_datasets(['sbrick']);
-load_datasets([
-	'sheets-00',
-	'sheets-01',
-	'sheets-02',
-	'sheets-03',
-	'sheets-04',
-	'sheets-05',
-	'sheets-06',
-	'sheets-07',
-	'sheets-08',
-	'sheets-09',
-]);
-load_datasets(['skilltree']);
-load_datasets(['visual_slot']);
+
+$groups = group_datasets(RYZOM_EXTRA_SHEETS_CACHE.'/*.serial');
+foreach($groups as $group => $files) {
+	load_datasets($files);
+}
 
 printf("% 15s bytes\n", number_format(memory_get_peak_usage(), 2, '.', ','));
 
